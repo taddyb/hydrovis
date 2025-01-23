ae01b450116f259fce4869bc842a0a18df270ca9
CatherineFitzpatrick-NOAA <49403615+cfitzpat479@users.noreply.github.com>
2025-01-23 08:25:47 -0600

    New service - Alaska AEP FIM (#1054)
    Implements AEP FIM - Alaska static service
    Refs #917

---
+++ Core/LAMBDA/viz_functions/viz_fim_data_prep/hand_features_sql/rf_high_water_inundation_ak.sql
@@ -0,0 +1,15 @@
+SELECT
+    crosswalk.hand_id,
+    max_forecast.feature_id,
+    CONCAT(LPAD(crosswalk.huc8::text, 8, '0'), '-', crosswalk.branch_id) as huc8_branch,
+    LEFT(LPAD(crosswalk.huc8::text, 8, '0'), 6) as huc,
+    crosswalk.hydro_id,
+    ROUND(CAST(max_forecast.high_water_threshold * 0.0283168 as numeric), 2) AS streamflow_cms
+FROM derived.recurrence_flows_ak AS max_forecast
+JOIN derived.fim4_featureid_crosswalk AS crosswalk ON max_forecast.feature_id = crosswalk.feature_id
+LEFT OUTER JOIN publish.rf_high_water_inundation_ak AS fim ON max_forecast.feature_id = fim.feature_id
+WHERE 
+    crosswalk.huc8 IS NOT NULL AND 
+    crosswalk.lake_id = -999 AND
+    fim.feature_id IS NULL AND 
+    crosswalk.branch_id != 0;
